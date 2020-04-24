f6ea82c297ae00e8e498794f6cc0798696402a19
deckerjs <squidrancher@gmail.com>
2020-04-24 05:51:04 -0600

    1st attempt of mongo serializer

--- GeoStoreAPI/DataAccess/MongoDataAccess/ApplicationBuilderMongoDbExtensions.cs
+++ GeoStoreAPI/DataAccess/MongoDataAccess/ApplicationBuilderMongoDbExtensions.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace GeoStoreAPI.Extensions
 {
-    public static class ApplicationBuilderExtensions
+    public static class ApplicationBuilderMongoDbExtensions
     {
         public static IApplicationBuilder InitializeMongo(this IApplicationBuilder app)
         {
@@ -18,14 +18,40 @@ namespace GeoStoreAPI.Extensions
         }
 
         private static void AddBsonMapConfigurations()
-        {
+        {   
+            // public MyClassSerializer : IBsonSerializer {
+            // implement Deserialize
+            // implement GetDefaultSerializationOptions
+            // implement Serialize
+            //}
+
+            //// register your custom serializer
+            //BsonSerializer.RegisterSerializer(
+            //typeof(MyClass),
+            //new MyClassSerializer()
+            //);
+
+
             if (!BsonClassMap.IsClassMapRegistered(typeof(GeoData)))
             {
+
+                //custom serializer
                 BsonClassMap.RegisterClassMap<GeoData>(cm =>
                 {
                     cm.AutoMap();
+                    cm.GetMemberMap(c => c.Data).SetSerializer(new CustomGeoJsonSerializer());
                     cm.SetIgnoreExtraElements(true);
                 });
+
+                //BsonClassMap.RegisterClassMap<GeoData>(cm =>
+                //{
+                //    cm.AutoMap();
+                //    //for readonly properties, since by default they will be ignored
+                //    //BsonClassMap.RegisterClassMap<GeoData>(cm => {
+                //    //    cm.MapProperty(c => c.SomeProperty);
+                //    //});
+                //    cm.SetIgnoreExtraElements(true);
+                //});
             }
             if (!BsonClassMap.IsClassMapRegistered(typeof(AppRole)))
             {
---
+++ GeoStoreAPI/DataAccess/MongoDataAccess/CustomGeoJsonSerializer.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Bson.Serialization;
+using System;
+using Newtonsoft.Json;
+using GeoDataModels.Models;
+
+namespace GeoStoreAPI.Extensions
+{
+    //, IBsonDocumentSerializer
+    //, IBsonIdProvider
+    internal class CustomGeoJsonSerializer : IBsonSerializer
+    {
+        public Type ValueType { get { return typeof(FeatureCollection); } }
+
+        //public bool GetDocumentId(object document, out object id, out Type idNominalType, out IIdGenerator idGenerator)
+        //{
+        //    var item = (GeoData)document;
+        //    id = item.ID;
+        //}
+
+        //public void SetDocumentId(object document, object id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool TryGetMemberSerializationInfo(string memberName, out BsonSerializationInfo serializationInfo)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public object Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var item = JsonConvert.DeserializeObject<FeatureCollection>(context.Reader.ReadString());
+            return item;
+        }
+
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        {
+            var item = JsonConvert.SerializeObject(value);
+            context.Writer.WriteString(item);
+        }
+
+
+    }
+}
