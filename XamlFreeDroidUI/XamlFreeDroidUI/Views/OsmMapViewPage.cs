d23699ddc0f221f57884836854932670fe78ab89
deckerjs <squidrancher@gmail.com>
2020-11-08 11:35:24 -0600

    load custom map from raster mbtiles file

--- XamlFreeDroidUI/XamlFreeDroidUI/Views/OsmMapViewPage.cs
+++ XamlFreeDroidUI/XamlFreeDroidUI/Views/OsmMapViewPage.cs
@@ -12,6 +12,12 @@ using Mapsui.Layers;
 using Mapsui.Geometries;
 using System.Linq;
 using Mapsui.Providers;
+using System.IO;
+using Mapsui.UI;
+using BruTile.MbTiles;
+using SQLite;
+using System.Reflection;
+using Xamarin.Essentials;
 
 namespace XamlFreeDroidUI.Views
 {
@@ -26,13 +32,16 @@ namespace XamlFreeDroidUI.Views
 
         private View GetContent()
         {
-            var map = new Map
-            {
-                CRS = "EPSG:3857",
-                Transformation = new MinimalTransformation()
-            };
+            // for online osm map
+            //var map = new Map
+            //{
+            //    CRS = "EPSG:3857",
+            //    Transformation = new MinimalTransformation()
+            //};
+            //map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            // open custom raster mbtiles map file
+            var map = GetCustomMap();
 
             var lineStringLayer = CreateLineStringLayer(CreateLineStringStyle());
             map.Layers.Add(lineStringLayer);
@@ -58,11 +67,8 @@ namespace XamlFreeDroidUI.Views
                     }
                 }
             };
-
             return stackLayout;
-
         }
-
 
         public static ILayer CreateLineStringLayer(IStyle style = null)
         {
@@ -83,13 +89,58 @@ namespace XamlFreeDroidUI.Views
             {
                 Fill = null,
                 Outline = null,
-                Line = { Color = Mapsui.Styles.Color.FromString("YellowGreen"), Width = 4 }
+                Line = { Color = Mapsui.Styles.Color.Green, Width = 4 }
             };
         }
 
-        private const string WKTGr5 = "LINESTRING(51.494894027709961 4.295525550842285, 51.494894027709961 4.295740127563477, 51.49467945098877 4.295697212219238, 51.494636535644531 4.295718669891357, 51.494615077972412 4.295804500579834, 51.494615077972412 4.29591178894043, 51.494593620300293 4.29614782333374, 51.494572162628174 4.296340942382813, 51.494572162628174 4.296555519104004, 51.494550704956055 4.296748638153076, 51.494486331939697 4.296748638153076, 51.494486331939697 4.297542572021484, 51.49442195892334 4.298250675201416, 51.494293212890625 4.299302101135254, 51.493842601776123 4.299130439758301, 51.49367094039917 4.299066066741943, 51.49341344833374 4.298958778381348, 51.492898464202881 4.298744201660156, 51.492919921875 4.29990291595459, 51.492941379547119 4.300224781036377, 51.493070125579834 4.300782680511475, 51.493113040924072 4.300975799560547, 51.493263244628906 4.301769733428955, 51.493349075317383 4.302220344543457, 51.493370532989502 4.302327632904053, 51.493391990661621 4.302434921264648, 51.49341344833374 4.302542209625244, 51.493434906005859 4.30264949798584, 51.493456363677979 4.302799701690674, 51.493477821350098 4.302949905395508, 51.493520736694336 4.303250312805176, 51.493563652038574 4.303486347198486, 51.493606567382813 4.303679466247559, 51.493649482727051 4.303872585296631, 51.493713855743408 4.304172992706299, 51.493778228759766 4.304580688476563, 51.493864059448242 4.305438995361328, 51.493885517120361 4.3056321144104, 51.493949890136719 4.306426048278809, 51.493992805480957 4.306769371032715, 51.494078636169434 4.307692050933838, 51.494078636169434 4.307881146669388, 51.494078636169434 4.308078289031982, 51.493713855743408 4.308056831359863, 51.493628025054932 4.308078289031982, 51.49341344833374 4.308185577392578, 51.493370532989502 4.308185577392578, 51.493327617645264 4.30814266204834, 51.493220329284668 4.30788516998291, 51.493155956268311 4.307799339294434, 51.492340564727783 4.307734966278076, 51.491696834564209 4.307241439819336, 51.491267681121826 4.306812286376953)";
+        private const string WKTGr5 = "LINESTRING(40.939812306314707 -104.4140350073576, 40.770964482799172 -105.78512543812394, 40.884347157552838 -105.83570159040391, 40.804107449948788 -105.66309272311628)";
 
+        private static Mapsui.Map GetCustomMap()
+        {
+            var map = new Mapsui.Map();
+
+            //todo: decide which location is better for an included basemap
+            //Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var basepath = @"." + Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            //var file = "world.mbtiles";
+            var file = "co-test.mbtiles";
+            var fullpath = Path.Combine(basepath, file);
+                        
+            //var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(@"XamlFreeDroidUI.world.mbtiles");
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(@"XamlFreeDroidUI.co-test-3.mbtiles");
+
+
+            using (BinaryReader br = new BinaryReader(stream))
+            {
+                using (BinaryWriter bw = new BinaryWriter(new FileStream(fullpath, FileMode.Create)))
+                {
+                    byte[] buffer = new byte[2048];
+                    int len = 0;
+                    while ((len = br.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        bw.Write(buffer, 0, len);
+                    }
+                }
+            }
+
+            if (File.Exists(fullpath))
+            {
+                map.Layers.Add(CreateMbTilesLayer(fullpath, "regular"));
+            }
+            else
+            {
+                throw new Exception($"invalid file path: {fullpath}");
+            }               
+
+            return map;
+        }
+
+        public static TileLayer CreateMbTilesLayer(string path, string name)
+        {
+            var sqliteconn = new SQLiteConnectionString(path,  false);
+            var mbTilesTileSource = new MbTilesTileSource(sqliteconn);
+            var mbTilesLayer = new TileLayer(mbTilesTileSource) { Name = name };
+            return mbTilesLayer;
+        }
     }
-
-
 }
